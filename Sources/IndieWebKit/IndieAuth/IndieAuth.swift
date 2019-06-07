7ebe43f4d23a3a790e41748711ed47e93557c3f4
Edward Hinkle <ehinkle@threatconnect.com>
2019-06-07 16:31:57 -0400

    Finish building out isValidProfile with passing tests

--- Sources/IndieWebKit/IndieAuth/IndieAuth.swift
+++ Sources/IndieWebKit/IndieAuth/IndieAuth.swift
@@ -6,48 +6,133 @@
 //
 
 import Foundation
+import Network
 
 public class IndieAuth {
     
+    /// Convienance method to check if a given string is a valid IndieAuth profile
+    /// - Parameter string: a string representation of an IndieAuth url profile
     static public func isValidProfile(string: String) -> Bool {
-        let urlOfString = URL(string: string);
         
-        guard let url = urlOfString else {
+        guard let url = URL(string: string) else {
+            // An IndieAuth profile is not valid if it's not a url
             return false;
         }
 
         return IndieAuth.isValidProfile(url: url);
     }
     
+    /// Checks if a given url is valid for the purpose of representing an IndieAuth profile
+    /// Logic for a valid profile comes from ths spec here: https://indieauth.spec.indieweb.org/#user-profile-url
+    /// - Parameter url: an IndieAuth profile url
     static public func isValidProfile(url: URL) -> Bool {
-        return false;
-        
-        
-        
+        // If the url is so badly formed it can't be turned into components, it definitely isn't valid
+        guard let urlComp = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
+            return false;
+        }
         
         // Profile URLs MUST have either an https or http scheme
+        guard urlComp.scheme != nil && (urlComp.scheme == "http" || urlComp.scheme == "https") else {
+            return false;
+        }
+        
         // MUST contain a path component (/ is a valid path),
+        guard urlComp.path != "" else {
+            return false;
+        }
+        
         // MUST NOT contain single-dot or double-dot path segments
+        guard !urlComp.path.contains(".") && !urlComp.path.contains("..") else {
+            return false;
+        }
+        
         // MAY contain a query string component
+        // I don't think we need to do anything special to check this
+        
         // MUST NOT contain a fragment component
+        guard urlComp.fragment == nil else {
+            return false;
+        }
+        
         // MUST NOT contain a username or password component
+        guard urlComp.user == nil && urlComp.password == nil else {
+            return false;
+        }
+        
         // MUST NOT contain a port.
+        guard urlComp.port == nil else {
+            return false;
+        }
+        
         // Additionally, hostnames MUST be domain names and MUST NOT be ipv4 or ipv6 addresses.
-        //
-        //        Some examples of valid profile URLs are:
-        //
-        //        https://example.com/
-        //        https://example.com/username
-        //        https://example.com/users?id=100
-        //        Some examples of invalid profile URLs are:
-        //
-        //        example.com - missing scheme
-        //        mailto:user@example.com - invalid scheme
-        //        https://example.com/foo/../bar - contains a double-dot path segment
-        //        https://example.com/#me - contains a fragment
-        //        https://user:pass@example.com/ - contains a username and password
-        //        https://example.com:8443/ - contains a port
-        //        https://172.28.92.51/ - host is an IP address
+        if #available(OSX 10.14, *) {
+            guard let hostname = urlComp.host else {
+                return false;
+            }
+            
+            guard IPv4Address(hostname) == nil || hostname == "127.0.0.1" else {
+                return false;
+            }
+            
+            guard IPv6Address(hostname) == nil || hostname == "[::1]" else {
+                return false;
+            }
+        }
+        
+        // If we made it here, we must have a legit profile! 🙌
+        return true;
     }
+    
+    /// Convienance method to normalize a given string and convert it into a Url for use as an IndieAuth Profile
+    /// - Parameter string: a string representation of an IndieAuth url profile
+//    static public func normalizeProfileUrl(string: String) -> URL? {
+//
+//        guard let url = URL(string: string) else {
+//            // An IndieAuth profile is not valid if it's not a url
+//            return nil;
+//        }
+//
+//        return IndieAuth.normalizeProfileUrl(url: url);
+//    }
+    
+    /// Normalize the profile url based on IndieAuth spec
+    /// https://indieauth.spec.indieweb.org/#url-canonicalization
+    /// - Parameter url: an IndieAuth profile url
+//    static public func normalizeProfileUrl(url: URL) -> URL? {
+//        guard let originalUrl = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
+//            return nil;
+//        }
+//
+//        var normalizedUrl = URLComponents()
+//
+//        normalizedUrl.host = originalUrl.host
+//        normalizedUrl.query = originalUrl.query
+//
+//        // If no scheme exists, add http
+//        if originalUrl.scheme == nil {
+//            normalizedUrl.scheme = "http"
+//        } else {
+//            normalizedUrl.scheme = originalUrl.scheme
+//        }
+//
+//        if originalUrl.path {
+//            normalizedUrl.path = originalUrl.path
+//        } else {
+//            normalizedUrl.path = "/"
+//        }
+//
+//        // If path is empty, we append / as the path
+////        if originalUrl.path || originalUrl.path == "" {
+////            normalizedUrl.path = "/"
+////        } else {
+////            normalizedUrl.path = originalUrl.path
+////        }
+//
+//        return normalizedUrl.url
+//    }
+    
+//    static public func discoverProfileEndpoints(url: URL) -> void {
+//        // TODO: Follow https://indieauth.spec.indieweb.org/#discovery-by-clients
+//    }
     
 }
--- Tests/IndieWebKitTests/IndieAuthTests.swift
+++ Tests/IndieWebKitTests/IndieAuthTests.swift
@@ -7,20 +7,26 @@ final class IndieAuthTests: XCTestCase {
         let valid_profile_1 = "https://example.com/"
         let valid_profile_2 = "https://example.com/username"
         let valid_profile_3 = "https://example.com/users?id=100"
+        let valid_profile_4 = "http://127.0.0.1/";
+        let valid_profile_5 = "http://[::1]/";
 
 
         XCTAssertEqual(IndieAuth.isValidProfile(string: valid_profile_1), true)
         XCTAssertEqual(IndieAuth.isValidProfile(string: valid_profile_2), true)
         XCTAssertEqual(IndieAuth.isValidProfile(string: valid_profile_3), true)
+        XCTAssertEqual(IndieAuth.isValidProfile(string: valid_profile_4), true)
+        XCTAssertEqual(IndieAuth.isValidProfile(string: valid_profile_5), true)
     }
 
     func testInvalidProfileUrls() {
-        let invalid_profile_1 = "example.com"
-        let invalid_profile_2 = "mailto:user@example.com"
-        let invalid_profile_3 = "https://example.com/foo/../bar"
-        let invalid_profile_4 = "https://example.com/#me"
-        let invalid_profile_5 = "https://user:pass@example.com/"
-        let invalid_profile_6 = "https://172.28.92.51/"
+        let invalid_profile_1 = "example.com" // missing scheme
+        let invalid_profile_2 = "mailto:user@example.com" // invalid scheme
+        let invalid_profile_3 = "https://example.com/foo/../bar" // contains a double-dot path
+        let invalid_profile_4 = "https://example.com/#me" // contains a gragment
+        let invalid_profile_5 = "https://user:pass@example.com/" // contains username and password
+        let invalid_profile_6 = "https://172.28.92.51/" // host is an IPv4 address
+        let invalid_profile_7 = "https://2001:0db8:85a3:0000:0000:8a2e:0370:7334/" // host is an IPv6 Address
+        let invalid_profile_8 = "https://example.com/foo/./bar" // contains a single-dot path
 
 
         XCTAssertEqual(IndieAuth.isValidProfile(string: invalid_profile_1), false)
@@ -29,10 +35,19 @@ final class IndieAuthTests: XCTestCase {
         XCTAssertEqual(IndieAuth.isValidProfile(string: invalid_profile_4), false)
         XCTAssertEqual(IndieAuth.isValidProfile(string: invalid_profile_5), false)
         XCTAssertEqual(IndieAuth.isValidProfile(string: invalid_profile_6), false)
+        XCTAssertEqual(IndieAuth.isValidProfile(string: invalid_profile_7), false)
+        XCTAssertEqual(IndieAuth.isValidProfile(string: invalid_profile_8), false)
     }
     
+//    func testNormalizeHostnameUrl() {
+//        let hostname = "example.com";
+//
+//        XCTAssertEqual(IndieAuth.normalizeProfileUrl(string: hostname), URL(string: "http://example.com/"))
+//    }
+    
     static var allTests = [
-        ("verifyValidProfileUrls", testValidProfileUrls),
-        ("verifyInvalidProfileUrls", testInvalidProfileUrls),
+        ("Test Valid Profile Urls", testValidProfileUrls),
+        ("Test Invalid Profile Urls", testInvalidProfileUrls),
+//        ("Test Normalized Profile Urls", testNormalizeHostnameUrl),
     ]
 }
