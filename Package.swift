1ac251a3f1bb6be7dbb5d40ffbacf49a1f75e07b
Eddie Hinkle <eddie@eddiehinkle.com>
2019-06-09 06:36:48 -0400

    Add SwiftSoup dependency

--- Package.swift
+++ Package.swift
@@ -1,10 +1,14 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "IndieWebKit",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v12),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -14,13 +18,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", .upToNextMajor(from: "2.2.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "IndieWebKit",
-            dependencies: []),
+            dependencies: ["SwiftSoup"]),
         .testTarget(
             name: "IndieWebKitTests",
             dependencies: ["IndieWebKit"]),
